b6d93766ab3eb27f033b88b4b847ba5b679f7655
hakuginkyubi <hakuginkyubi@gmail.com>
2013-06-28 15:23:09 +0900

    再度

--- Jframe/src/Main.java
+++ Jframe/src/Main.java
@@ -3,59 +3,13 @@ import net.miginfocom.swing.MigLayout;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
-import javax.swing.JPanel;
-
 import java.awt.Dimension;
-import java.awt.Panel;
-import java.awt.ScrollPane;
 import javax.swing.JTable;
-import javax.swing.JScrollBar;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.JButton;
 import javax.swing.ScrollPaneConstants;
+
 public class Main extends JFrame {
-	public JTable table;
-	private DefaultTableModel tm;
-	public Main() {
-		setTitle("メッセージ");
-		getContentPane().setLayout(new MigLayout("", "[grow,grow][grow,grow]", "[27.00][27.00][30.00][grow,grow]"));
-		
-		JLabel label = new JLabel("名前：");
-		getContentPane().add(label, "cell 0 0,alignx trailing");
-		
-		textField = new JTextField();
-		getContentPane().add(textField, "cell 1 0,grow");
-		textField.setColumns(10);
-		
-		JLabel label_1 = new JLabel("メッセージ：");
-		getContentPane().add(label_1, "cell 0 1,alignx trailing");
-		
-		textField_1 = new JTextField();
-		getContentPane().add(textField_1, "cell 1 1,grow");
-		textField_1.setColumns(10);
-		
-		//JTableの作り方
-				//テーブルに設定する列名の作成
-				String[] header = {"名前","メッセージ"};
-				 tm = new DefaultTableModel(header,0);
-				  
-				  btnNewButton = new JButton("～送信～");
-				  btnNewButton.setToolTipText("");
-				  getContentPane().add(btnNewButton, "cell 0 2 2 1,grow");
-				 //JTableの作成
-				  table = new JTable(tm);
-				  table.setPreferredSize(new Dimension (512,512));
-				  //作成したJTableにスクロール機能を設定
-				  JScrollPane scroll = new JScrollPane(table, 
-				          JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, 
-				          ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
-				  scroll.setToolTipText("");
-				  getContentPane().add(scroll, "cell 0 3 2 1,grow");
-	}
-	private static final long serialVersionUID = 1L;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JButton btnNewButton;
 	// メインメソッド
 	public static void main(String[] args) {
 		// 真っ白のウィンドウを表示するだけ
@@ -64,5 +18,49 @@ public class Main extends JFrame {
 		mainFrame.setSize(360, 240);
 		mainFrame.setLocationRelativeTo(null);
 		mainFrame.setVisible(true);
+	}
+	private static final long serialVersionUID = 1L;
+	private JTextField NameField;
+	private JTextField MessageField;
+	private JButton btnNewButton;
+	public JTable table;
+	private DefaultTableModel tm;
+	
+	public Main() {
+		setTitle("メッセージ");
+		getContentPane().setLayout(new MigLayout("", "[480,grow][480,grow]", "[27.00][27.00][30.00][480,grow]"));
+
+		JLabel Namelabel = new JLabel("名前：");
+		getContentPane().add(Namelabel, "cell 0 0,alignx trailing");
+
+		NameField = new JTextField();
+		getContentPane().add(NameField, "cell 1 0,grow");
+		NameField.setColumns(10);
+
+		JLabel Messagelabel = new JLabel("メッセージ：");
+		getContentPane().add(Messagelabel, "cell 0 1,alignx trailing");
+
+		MessageField = new JTextField();
+		getContentPane().add(MessageField, "cell 1 1,grow");
+		MessageField.setColumns(10);
+
+		//JTableの作り方
+		//テーブルに設定する列名の作成
+		String[] header = {"名前","メッセージ"};
+		tm = new DefaultTableModel(header,0);
+
+		btnNewButton = new JButton("～送信～");
+		btnNewButton.setToolTipText("");
+		getContentPane().add(btnNewButton, "cell 0 2 2 1,grow");
+		//JTableの作成
+		table = new JTable(tm);
+		table.setPreferredSize(new Dimension (512,512));
+		//作成したJTableにスクロール機能を設定
+		JScrollPane scroll = new JScrollPane(table, 
+				JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, 
+				ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+		scroll.setToolTipText("");
+		getContentPane().add(scroll, "cell 0 3 2 1,grow");
+
 	}
 }
